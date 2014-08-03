a91f70501ad70b1045f3758b3569f4353be05bcf
k8 <karagdon@gmail.com>
2014-08-02 18:46:09 -0700

    Binary Represenation in base 2, 8, 16

---
+++ Python_codeAcademy/14.py
@@ -0,0 +1,11 @@
+
+cleanList = []
+
+def remove_duplicates(checkList):
+    
+    for i in checklist:
+        if i not in cleanList:        
+            cleanList.append(i)
+    return cleanList
+
+print remove_duplicates([1,2,3,1,2,3,1,2,3,4])
---
+++ Python_codeAcademy/binary_rep.py
@@ -0,0 +1,22 @@
+#bitwise represenation
+
+#Base 2:
+print bin(1)
+print bin(2)
+print bin(3)
+print bin(4)
+print bin(5)
+
+#base8
+print hex(7)
+print hex(6)
+print hex(8)
+print hex(9)
+print hex(10)
+
+#base 16
+print oct(11)
+print oct(12)
+print oct(13)
+print oct(14)
+print oct(15)
