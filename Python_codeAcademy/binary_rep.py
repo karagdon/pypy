0b542d8c1c7c733e5babe8f2effbe68a9649e6ae
k8 <karagdon@gmail.com>
2014-08-06 22:10:25 -0700

    BITWISE OPERATOR OVERVIEW #1

--- Python_codeAcademy/binary_rep.py
+++ Python_codeAcademy/binary_rep.py
@@ -10,4 +10,21 @@ print hex(7)
 print oct(11)
 
 
+#BITWISE OPERATORS#
+
+
+# THIS OR THAT >> A|B
+
+#The bitwise OR (|) operator compares two numbers on a bit level and returns a number where the bits of that number are turned on if either of the corresponding bits of either number are 1.
+
+# 0b1110
+# 0b0101
+# ======
+# 0b1111
+
+print "bin(0b1110|0b0101)"
+print bin(0b1110|0b0101)
+
+
+
 
