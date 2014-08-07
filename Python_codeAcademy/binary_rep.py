9fcab3c07d730a8bafc8fb3bf48e31745a8a5dae
k8 <karagdon@gmail.com>
2014-08-06 22:41:02 -0700

    BITWISE OPERATOR OVERVIEW - END OF OPERATORS

--- Python_codeAcademy/binary_rep.py
+++ Python_codeAcademy/binary_rep.py
@@ -66,22 +66,3 @@ print "~2 =", ~2
 print "~3 =", ~3
 print "~42 =", ~42
 print "~123 =", ~123
-
-# BIT MASK #
-
-# A bit mask is just a variable that aids you with bitwise operations. A bit mask can help you turn specific bits on, turn others off, or just collect data from an integer about which bits are on or off.
-
-# num  = 0b1100
-# mask = 0b0100
-# desired = num & mask
-# if desired > 0:
-#    print "Bit was on"
-
-def check_bit4(input):
-    mask = 0b1000
-    num = input
-    result ="on"
-    desired = num & mask
-    if desired < 1:
-        result = "off"
-    return result
