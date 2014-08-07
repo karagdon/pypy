db3a0570979a888fe6854ba9d454ae4df221f385
k8 <karagdon@gmail.com>
2014-08-06 22:43:50 -0700

    Masks - Turning on a binary digit with A|B

--- Python_codeAcademy/bit_masks.py
+++ Python_codeAcademy/bit_masks.py
@@ -17,3 +17,13 @@ def check_bit4(input):
         result = "off"
     return result
 
+#Turn It On
+# * with a mask
+
+#You can also use masks to turn a bit in a number on using |. For example, let's say I want to make sure the rightmost bit of number a is turned on.
+
+a = 0b10111011
+#Turn the 3rd bit on!
+b = 0b00000100
+
+print bin(a|b)
