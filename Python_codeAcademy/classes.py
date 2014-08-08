31db308ed7c9904ed5e1ee459f8b506194e6a2b9
k8 <karagdon@gmail.com>
2014-08-07 20:17:30 -0700

    Initializing Classes

---
+++ Python_codeAcademy/classes.py
@@ -0,0 +1,13 @@
+#By convention, user-defined Python class names start with a capital letter.
+class Animal(object):
+    #class magic here
+    pass
+  
+  #By convention, user-defined Python class names start with a capital letter.
+class Animal(object):
+    #class magic here
+    # this initializes the objects
+    def __init__(self, name):
+        #instantiate the object self with the parameter name
+        self.name = name
+
