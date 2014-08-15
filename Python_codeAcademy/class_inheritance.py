94e7a3cce5eb2de3d4de0c194fb45080551a72eb
k8 <karagdon@gmail.com>
2014-08-14 22:08:08 -0700

    Adding my own classes

--- Python_codeAcademy/class_inheritance.py
+++ Python_codeAcademy/class_inheritance.py
@@ -16,3 +16,22 @@ monty_python.display_cart()
 monty_python.display_order_history()
 
 #Inheritance is the process by which one class takes on the attributes and methods of another, and it's used to express an is-a relationship.
+
+#In Python, inheritance works like this:
+
+#class DerivedClass(BaseClass):
+#   # code goes here
+#where DerivedClass is the new class you're making and BaseClass is the class from which that new class inherits.
+#
+#class Shape(object):
+#    """Makes shapes!"""
+#    def __init__(self, number_of_sides):
+#       self.number_of_sides = number_of_sides
+
+# Add your Triangle class below!
+
+class Triangle(Shape):
+    def __init__(self,side1,side2,side3):
+        self.side1 = side1
+        self.side2 = side2
+        self.side3 = side3
