0b4a4237d1d92880d9cc852508c443ef9a2814da
k8 <karagdon@gmail.com>
2014-08-18 22:22:42 -0700

    creating a car class

---
+++ Python_codeAcademy/class_car.py
@@ -0,0 +1,22 @@
+#class Car(object):
+#    pass
+#############################################
+#class Car(object):
+#    pass
+#my_car = Car()
+#############################################
+#class Car(object):
+#    pass
+#my_car = Car()
+#############################################
+#class Car(object):
+#    #This is a member variable, which stores information that #belongs to the class object
+#    condition = "new"
+#my_car = Car()
+#############################################
+#class Car(object):
+#    #This is a member variable, which stores information that belongs to the class object
+#    condition = "new"
+#my_car = Car()
+#print my_car.condition
+#############################################
