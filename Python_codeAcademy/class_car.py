935ae8cbda1b1c9abe90f6304aaa18390405a42a
k8 <karagdon@gmail.com>
2014-08-18 22:24:01 -0700

    adding some attributes as arguments

--- Python_codeAcademy/class_car.py
+++ Python_codeAcademy/class_car.py
@@ -1,22 +1,13 @@
-#class Car(object):
-#    pass
-#############################################
-#class Car(object):
-#    pass
-#my_car = Car()
-#############################################
-#class Car(object):
-#    pass
-#my_car = Car()
-#############################################
-#class Car(object):
-#    #This is a member variable, which stores information that #belongs to the class object
-#    condition = "new"
-#my_car = Car()
-#############################################
-#class Car(object):
-#    #This is a member variable, which stores information that belongs to the class object
-#    condition = "new"
-#my_car = Car()
+
+class Car(object):
+#This is a member variable, which stores information that belongs to the class object
+    condition = "new"
+    def __init__(self, model, color, mpg):
+        self.model = model
+        self.color = color
+        self.mpg = mpg
+        return
+#my_car = Car("DeLorean", "silver", 88)
 #print my_car.condition
-#############################################
+mycar = Car('DeLorean','silver',88)
+print mycar.condition, mycar.model, mycar.color, mycar.mpg
