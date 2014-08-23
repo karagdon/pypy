4cefb000c9f6c2c7a53acbfcd7a98e4bfb845e07
k8 <karagdon@gmail.com>
2014-08-22 18:53:57 -0700

    final: buffering, with and as, is file closed

--- Python_codeAcademy/open-write-close.py
+++ Python_codeAcademy/open-write-close.py
@@ -6,3 +6,33 @@ my_file = open("output.txt", "r+")
 for number in my_list:
     my_file.write(str(number) + "\n")
 my_file.close()
+
+###
+
+# Open the file for reading
+read_file = open("text.txt", "r")
+
+# Use a second file handler to open the file for writing
+write_file = open("text.txt", "w")
+# Write to the file
+write_file.write("Not closing files is VERY BAD.")
+
+write_file.close()
+read_file.close()
+# Try to read from the file
+print read_file.read()
+
+
+# With/AS #
+
+with open("text.txt", "w") as textfile:
+	textfile.write("Success!")
+
+#See if it is closed#
+with open("text.txt", "w") as my_file:
+	my_file.write("Success!")
+	if my_file.closed == 1:
+	    my_file.close()
+	    
+print my_file.closed
+	
