11f4e61ebfeee856dbed09071d1459a77f3756fc
k8 <karagdon@gmail.com>
2014-08-24 23:17:17 -0700

    Maybe start by renaming a file

--- first_test/pything.py
+++ first_test/pything.py
@@ -1,2 +1,11 @@
 #starting new code
-print "hello world!"
+print "hello world"
+
+#maybe we can start with a file renaming thing
+import os, sys
+
+# listing directories
+print "The dir is: %s"%os.listdir(os.getcwd())
+
+# renaming directory ''tutorialsdir"
+os.rename("tutorialsdir","tutorialsdirectory")
