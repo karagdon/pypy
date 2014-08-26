776aa07145ec2ee94bf0c5d94c1e0b5c4b75ce8f
k8 <karagdon@gmail.com>
2014-08-25 19:37:21 -0700

    testing renaming a file

--- first_test/pything.py
+++ first_test/pything.py
@@ -9,3 +9,6 @@ print "The dir is: %s"%os.listdir(os.getcwd())
 
 # renaming directory ''tutorialsdir"
 os.rename("tutorialsdir","tutorialsdirectory")
+
+# renaming file "aa1.txt"
+os.renames("aa1.txt","newdir/aanew.txt")
