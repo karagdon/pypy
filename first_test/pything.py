a013773886fe8d977f6e9d5f62e00ab3b954cfa3
k8 <karagdon@gmail.com>
2014-08-26 20:21:44 -0700

    changed a file name!

--- first_test/pything.py
+++ first_test/pything.py
@@ -1,15 +1,14 @@
 #starting new code
-refile= raw_input('hello world, what file do you want to rename?')
-
+refile = raw_input('hello world, what file do you want to rename?')
 
 #maybe we can start with a file renaming thing
 import os, sys
 
 # listing directories
-print "The dir is: %s"%os.listdir(os.getcwd())
+print "The dir has: %s"%os.listdir(os.getcwd())
 
 # renaming directory ''tutorialsdir"
 os.rename("tutorialsdir","tutorialsdirectory")
 
 # renaming file "aa1.txt"
-os.renames("aa1.txt","newdir/aanew.txt")
+os.renames(refile,"newdir/aanew.txt")
