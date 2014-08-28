99a9461283c7e5b08b7c91f5a285f1334f853f68
k8 <karagdon@gmail.com>
2014-08-27 20:31:00 -0700

    changing from two queries to a single cli command

--- first_test/pything.py
+++ first_test/pything.py
@@ -1,14 +1,22 @@
 #starting new code
-refile = raw_input('hello world, what file do you want to rename?')
+
 
 #maybe we can start with a file renaming thing
 import os, sys
+from sys import argv
+refile, newfile = argv
 
-# listing directories
+# list the director
 print "The dir has: %s"%os.listdir(os.getcwd())
 
+# refile = raw_input('hello world, what file do you want to rename?')
+# newfile = raw_input('what should we name it to?')
+
+
 # renaming directory ''tutorialsdir"
-os.rename("tutorialsdir","tutorialsdirectory")
+#os.rename("tutorialsdir","tutorialsdirectory")
 
 # renaming file "aa1.txt"
-os.renames(refile,"newdir/aanew.txt")
+os.renames(refile, newfile)
+
+print "File(s) successfully renamed. The dir now has: %s"%os.listdir(os.getcwd())
