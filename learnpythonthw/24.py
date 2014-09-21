0b55336e948503bafb768e50f8f04dd991014663
karagdon <karagdon@gmail.com>
2014-09-20 22:09:30 -0700

    Daily code

--- learnpythonthw/24.py
+++ learnpythonthw/24.py
@@ -1,5 +1,5 @@
-print "Let's pracetice everything."
-print 'You\'d need to know \'bout escapes with \\ that do \n newlines \t tabs.'
+print "Let's practice everything."
+print 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.'
 
 poem = """
 \tThe lovely world
