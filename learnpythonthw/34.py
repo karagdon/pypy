0f4f41605efe6b9dee0f321d39f9acf675b46ddf
karagdon <karagdon@gmail.com>
2014-09-26 23:14:55 -0700

    Daily code

---
+++ learnpythonthw/34.py
@@ -0,0 +1,12 @@
+animals = ['bear', 'tiger', 'penguin', 'zebra','cat','dog']
+bear = animals[0]
+print animals
+print "............"
+print "The animal at 1 is %s" % animals[1]
+print "The third (3rd) animal. is %s" % animals[2]
+print "The first (1st) animal. is %s" % animals[0]
+print "The animal at 3. is %s" % animals[3]
+print "The fifth (5th) animal. is %s" % animals[4]
+print "The animal at 2. is %s" % animals[2]
+print "The sixth (6th) animal. is %s" % animals[5]
+print "The animal at 4. is %s" % animals[4]
