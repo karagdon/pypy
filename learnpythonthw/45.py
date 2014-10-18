42a659971a9140155c1d4060ecb239d2e6bdc39b
karagdon <karagdon@gmail.com>
2014-10-17 23:38:55 -0700

    daily code

--- learnpythonthw/45.py
+++ learnpythonthw/45.py
@@ -7,21 +7,22 @@ print "Welcome to the PETSHOP OF HORRORS, what kind of pet would you be interest
 
 #class Type(self):
 petType =  {
-  'A' : "Amphibian",
-  'B' : "Bird",
-  'C' : "Cat",
-  'D' : "Dog",
-  'E' : "Elephant"
+	'A' : "Amphibian",
+	'B' : "Bird",
+	'C' : "Cat",
+	'D' : "Dog",
+	'E' : "Elephant"
 }
 choosePetType = raw_input("> ")
 choosePetType = choosePetType.upper()
 choosePetType = choosePetType.strip()
 
-if choosePetType in petType:
-  print "%ss, great choice... one second... *cackle*" % (petType[choosePetType])
-else:
-  print "We don't have that. Try again tomorrow."
 
+if choosePetType in petType:
+	print "%ss, great choice... one second... *cackle*" % (petType[choosePetType])
+else:
+	print "We don't have that. Try again tomorrow."
+	raise SystemExit
 
 #    def Color(self):
 colorHash = {
@@ -53,30 +54,69 @@ chooseColor = chooseColor.strip()
 if chooseColor in haveColors:
 	print "Shop owner: You want that %s %s? There are *NO* returns. \n * [Y]es\n * [N]o" % (colorHash[chooseColor], (petType[choosePetType]))
  	purchased = raw_input("> ")
+	purchased = purchased.lower()
+	purchased = purchased.strip()
 else:
 	print "Shop owner: We don't have that, maybe tomorrow."
+	raise SystemExit
 
-purchased = purchased.lower()
-purchased = purchased.strip()
 
 #def name(self):
 if purchased == 'y':
 	print "You spend all your money to buy the pet. You are now broke. The shop owner cackles as you exit the store. Meanwhile, you decide on your new pet's name..."
 	name = raw_input("> ")
-	print "Interesting choice for a name.."
+	print "Shop owner: Interesting choice for a name.."
 else:
 	print "Shop owner: Quit wasting my time, Halloween is coming!"
+	raise SystemExit
+	
+#def scare():
+# Add scareScore for pet type
+scareScore = colorHash[chooseColor] + (petType[choosePetType])
+points = 0
+scareScore = scareScore.lower()
+scareScore = scareScore.strip()
+scareScore += scareScore.upper()
+for s in name:
+	if s in scareScore:
+		points += 1
+print points
+print len(name)
+
+randDeath = [
+	"set fire to your hair",
+	"poke a stick at a grizzly bear",
+	"eat medicine that's out of date",
+	"use your private parts as piranha bait",
+	"teach yourself how to fly",
+	"eat a two week old unrefrigerated pie",
+	"invite a psycho killer inside",
+	"scratch a drug dealer's brand new ride",
+	"eat a tube of superglue",
+	"disturb a nest of wasps for no good reason",
+	"stand on the edge of a train station platform"
+	]
+randDeath = random.sample(randDeath, 1)
+if points >= (len(name)):
+	print "%s does not kill you as you approach it" % name
+else:
+	print "%s hates you, and terrorizes the city. You also %s and die. GG." % (name,randDeath)
 
 
-'''
+print points
+print "".join(set(scareScore))
+
+
+
+
+#print "Your scare score is: %s" % scareScore
+"""
 scare trigger: analyze the name, if the pet 'likes' the name, the pet will not eat you and you live(for now), if it does not, it will eat you.
 mood = good | bad
 	good = likes the name
 	bad = does not like the name
-	
 scareTrigger = 1
-'''
-
+"""
 
 """
 Goal:
