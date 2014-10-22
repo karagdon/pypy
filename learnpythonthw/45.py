e24dff87bf2d967d32f5e4cc914735b2a0eb043f
karagdon <karagdon@gmail.com>
2014-10-21 22:56:12 -0700

    dailycode

--- learnpythonthw/45.py
+++ learnpythonthw/45.py
@@ -5,72 +5,74 @@ print "Welcome to the PETSHOP OF HORRORS, what kind of pet would you be interest
 
 class Pet(object):
     def __init__(self):
-		pass
+		description = "cute"
+		return
+def Type(self):
+	petType =  {
+		'A' : "Amphibian",
+		'B' : "Bird",
+		'C' : "Cat",
+		'D' : "Dog",
+		'E' : "Elephant"
+	}
+	choosePetType = raw_input("> ")
+	choosePetType = choosePetType.upper()
+	choosePetType = choosePetType.strip()
+	
+	
+	if choosePetType in petType:
+		print "%ss, great choice... one second... *cackle*" % (petType[choosePetType])
+	else:
+		print "We don't have that. Try again tomorrow."
+		raise SystemExit
 
-class Type(self):
-petType =  {
-	'A' : "Amphibian",
-	'B' : "Bird",
-	'C' : "Cat",
-	'D' : "Dog",
-	'E' : "Elephant"
-}
-choosePetType = raw_input("> ")
-choosePetType = choosePetType.upper()
-choosePetType = choosePetType.strip()
-
-
-if choosePetType in petType:
-	print "%ss, great choice... one second... *cackle*" % (petType[choosePetType])
-else:
-	print "We don't have that. Try again tomorrow."
-	raise SystemExit
-
+	return petType
+"""
 def Color(self):
-colorHash = {
-        'r' : 'R]ed',
-        'o' : 'O]range',
-        'y' : 'Y]ellow',
-        'g' : 'G]reen',
-        'b' : 'B]lue',
-        'i' : 'I]ndigo',
-        'v' : 'V]iolet',
-        'w' : 'W]hite',
-        'gr' : 'Gr]ay',
-        'bl' : 'Bl]ack',
-        'br' : 'Br]own'
-    }
-    
-import random
-haveColors = random.sample(colorHash, 5)
-print "Shop owner: We have %ss in these colors today:" % petType[choosePetType]
+	colorHash = {
+	        'r' : 'R]ed',
+	        'o' : 'O]range',
+	        'y' : 'Y]ellow',
+	        'g' : 'G]reen',
+	        'b' : 'B]lue',
+	        'i' : 'I]ndigo',
+	        'v' : 'V]iolet',
+	        'w' : 'W]hite',
+	        'gr' : 'Gr]ay',
+	        'bl' : 'Bl]ack',
+	        'br' : 'Br]own'
+	    }
+	    
+	import random
+	haveColors = random.sample(colorHash, 5)
+	print "Shop owner: We have %ss in these colors today:" % petType[choosePetType]
+	
+	for i in haveColors:
+	    print " * ["+colorHash[i]
+	    colorHash[i] = colorHash[i].replace("]", "")
+	
+	chooseColor = raw_input("> ")
+	chooseColor = chooseColor.lower()
+	chooseColor = chooseColor.strip()
+	
+	if chooseColor in haveColors:
+		print "Shop owner: You want that %s %s? There are *NO* returns. \n * [Y]es\n * [N]o" % (colorHash[chooseColor], (petType[choosePetType]))
+	 	purchased = raw_input("> ")
+		purchased = purchased.lower()
+		purchased = purchased.strip()
+	else:
+		print "Shop owner: We don't have that, maybe tomorrow."
+		raise SystemExit
 
-for i in haveColors:
-    print " * ["+colorHash[i]
-    colorHash[i] = colorHash[i].replace("]", "")
 
-chooseColor = raw_input("> ")
-chooseColor = chooseColor.lower()
-chooseColor = chooseColor.strip()
-
-if chooseColor in haveColors:
-	print "Shop owner: You want that %s %s? There are *NO* returns. \n * [Y]es\n * [N]o" % (colorHash[chooseColor], (petType[choosePetType]))
- 	purchased = raw_input("> ")
-	purchased = purchased.lower()
-	purchased = purchased.strip()
-else:
-	print "Shop owner: We don't have that, maybe tomorrow."
-	raise SystemExit
-
-
-#def name(self):
-if purchased == 'y':
-	print "You spend all your money to buy the pet. You are now broke. The shop owner cackles as you exit the store. Meanwhile, you decide on your new pet's name..."
-	name = raw_input("> ")
-	print "Shop owner: Interesting choice for a name.."
-else:
-	print "Shop owner: Quit wasting my time, Halloween is coming!"
-	raise SystemExit
+def name(self):
+	if purchased == 'y':
+		print "You spend all your money to buy the pet. You are now broke. The shop owner cackles as you exit the store. Meanwhile, you decide on your new pet's name..."
+		name = raw_input("> ")
+		print "Shop owner: Interesting choice for a name.."
+	else:
+		print "Shop owner: Quit wasting my time, Halloween is coming!"
+		raise SystemExit
 	
 #def scare():
 # Add scareScore for pet type
@@ -106,10 +108,11 @@ else:
 
 #print "Your scare score is: %s" % scareScore
 
-"""
+
 Goal:
 pet.type = petType
 pet.name = name
 pet.color = color
 pet.scare = scare
 """
+print pet.Type
