07908339523f97300b7fce165dde6319e26ba6a5
karagdon <karagdon@gmail.com>
2014-10-28 23:10:49 -0700

    daily code

--- learnpythonthw/45-2.py
+++ learnpythonthw/45-2.py
@@ -1,8 +1,17 @@
-#convert game to with classes
-class Pet(object):
-	pass
+#practice with classes
 
-class Cat(Animal):
-	def __init__(self,name):
+class Cat(object):
+	def __init__(self,name,sound):
 		## cats have a name
 		self.name = name
+		self.sound = sound
+		
+	def description(self):
+		print self.name
+	
+	def animalSound(self):
+		print self.sound
+	
+my_cat = Cat("whiskers", "meow!")
+my_cat.description()
+my_cat.animalSound()
