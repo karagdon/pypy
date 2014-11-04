bc5815d74e03e2666d67412e9d13530d82f24cae
karagdon <karagdon@gmail.com>
2014-11-03 23:43:39 -0800

    daily code

--- 48.py
+++ 48.py
@@ -2,8 +2,24 @@
 stuff = raw_input('> ')
 words = stuff.split()
 
+print "A TUPLE IS SIMPLY A LIST YOU CANT MODIFY"
+
 # lexicon tuples
 first_word = ('verb', 'go')
 second_word = ('direction', 'north')
 third_word = ('direction', 'west')
 sentence = [first_word, second_word, third_word]
+
+def convert_numbers(s):
+	try:
+		return int(s):
+	except ValueError:
+		return None
+		
+print """
+Create one small part of the test I give you \n
+Make sure it runs and fails so you know a test is actually confirming a feature works. \n
+Go to your source file lexicon.py and write the code that makes this test pass \n
+Repeat until you have implemented everything in the test
+"""
+
