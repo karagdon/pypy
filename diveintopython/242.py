bc3629cb44a05276e786e3f7c2adb2b7a6947dbd
karagdon <karagdon@gmail.com>
2014-11-15 20:34:26 -0800

    daily code

---
+++ diveintopython/242.py
@@ -0,0 +1,9 @@
+import odbchelper
+params = {"server:":"mpilgrim", "database": "master", "uid":"sa", "pwd":"secret"}
+print odbchelperbuildConnectionString(params)
+print odbchelper.buildConnectionString.__doc__
+
+import sys
+sys.path
+sys
+sys.path.append('/my/new/path')
