8903ccc6b89f55aa9ba8e507ce91a45fefbbe343
karagdon <karagdon@gmail.com>
2014-11-22 17:02:31 -0800

    daily code

--- diveintopython/605.py
+++ diveintopython/605.py
@@ -1,21 +1,8 @@
-
-Type "help", "copyright", "credits" or "license" for more information.
->>> logfile = open('test.log', 'w')
->>> logfile.write('test succeeded')
->>> logfile.close()
->>> print file('test.log').read()
-test succeeded
->>> logfile = open('test.log', 'a')
->>> logfile.close()
->>> print file('test.log.read()
-  File "<stdin>", line 1
-    print file('test.log.read()
-                              ^
-SyntaxError: EOL while scanning string literal
->>> print file('test.log).read()
-  File "<stdin>", line 1
-    print file('test.log).read()
-                               ^
-SyntaxError: EOL while scanning string literal
->>> print file('test.log').read()
+logfile = open('test.log', 'w')
+logfile.write('test succeeded')
+logfile.close()
+print file('test.log').read()
+logfile = open('test.log', 'a')
+logfile.close()
+print file('test.log').read()
 test succeeded
