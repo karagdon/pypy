a1378dd6e91fac79f314b3a4ded5f93b3d3e3905
karagdon <karagdon@gmail.com>
2014-12-01 22:40:06 -0800

    daily code

--- diveintopython/706.py
+++ diveintopython/706.py
@@ -1,7 +1,13 @@
 phonePattern = re.compile(r'^(\d{3}-(\d{3})-(\d{4})$')
 phonePattern.search('800-555-1212').groups()
 phonePattern.search('800-555-1212-1234)
+
 phonePattern = re.compile(r'^(\d{3})-(\d{4})-(\d+)$)
 phonePattern.search('800-555-1212-1234').groups()
 phonePattern.search('800 555 1212 1234')
 phonePattern.search('800-555-1212')
+
+phonePattern = re.compile(r'^(\d{3}-(\d{3})-(\d+)$')
+phonePattern.search('800-555-1212-1234').groups()
+phonePattern.search('800 555 1212 1234')
+phonePattern.search('800-555-1212)
