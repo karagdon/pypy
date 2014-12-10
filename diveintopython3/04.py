1bee73b3bbaa3a11dbd49654a8f7095abc3b12aa
karagdon <karagdon@gmail.com>
2014-12-09 23:51:43 -0800

    daily code

---
+++ diveintopython3/04.py
@@ -0,0 +1,6 @@
+import humansize
+#import(Python) is like require(Perl)
+#module.function ~ module::function
+
+print(humansize.approximate_size(4096, True))
+print(humansize.approximate_size.__doc__)
