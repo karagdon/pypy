4726d33b6452ced92af027c32e21fec38e6a399f
karagdon <karagdon@gmail.com>
2014-12-10 23:28:01 -0800

    daily code

---
+++ diveintopython3/2.01.py
@@ -0,0 +1,19 @@
+#Booleans are either True or False
+#NUMBERS can be integers, floats, fractions or even complex numbers
+#STRINGS are sequences of Unicode characters
+#BYTES and byte arrays
+#LISTS are ordered sequences of values
+#TUPLES are ordered, immutable sequences of values
+#SETS are unordered bags of values
+#DICTIONARIES are unordered bags of key-value pairs
+
+'''
+EVERYTHING IS AN OBJECT!
+
+'''
+#legacy issues: booleans can be treated as numbers LOL.
+
+True + True #equals 2
+True - False # 1
+True * False # 0
+True / False
