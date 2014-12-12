db09fd24ef7328f3fc2e564a26c12cf197515bef
karagdon <karagdon@gmail.com>
2014-12-11 23:15:10 -0800

    daily code

--- diveintopython3/2.01.py
+++ diveintopython3/2.01.py
@@ -12,8 +12,37 @@ EVERYTHING IS AN OBJECT!
 
 '''
 #legacy issues: booleans can be treated as numbers LOL.
-
+"True + True"
 True + True #equals 2
+"True - False"
 True - False # 1
+"True * False"
 True * False # 0
-True / False
+"True / False"
+
+'''
+COERCING INTEGERS TO FLOATS AND VICE-VERSA
+
+'''
+
+float(2)
+int(2.0)
+int(2.5)
+int(-2.5)
+1.12345678901234567890
+type(10000000000000000)
+
+
+'''
+NUMBERS IN A BOOLEAN CONTEXT
+
+'''
+
+def is_it_true(anything):
+	if anything:
+		print("yes, it's true")
+	else:
+		print("no, it's false")
+is_it_true(1)
+is_it_true(0)
+is_it_true(0.0)
