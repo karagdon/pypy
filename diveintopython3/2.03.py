76d4acaaf41edeeb460ed0a8c6c19de6debf6bfe
karagdon <karagdon@gmail.com>
2014-12-13 12:24:22 -0800

    daily code

--- diveintopython3/2.03.py
+++ diveintopython3/2.03.py
@@ -1,3 +1,6 @@
+'''
+# BASIC MATH OPS
+
 import fractions
 x = fractions.Fraction(1,3)
 x
@@ -5,7 +8,27 @@ x * 2
 fractions.Fraction(6,4)
 fractions.Fraction(0,0)
 
+# TRIG
+
 import math
 math.pi
 math.sin(math.pi / 2)
 math.tan(math.pi / 4)
+
+'''
+# Numbers in a boolean contezxt
+
+def is_it_true(anything):
+	if anything:
+		print("yes, it's true")
+	else:
+		print("no, its false")
+is_it_true(1)
+is_it_true(-1)
+is_it_true(0)
+is_it_true(0.1)
+is_it_true(0.0)
+import fractions
+is_it_true(fractions.Fraction(1, 2))
+is_it_true(fractions.Fraction(0, 1))
+
