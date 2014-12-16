5990ff78928fe96564b713e2ad451da5afd3cd1a
karagdon <karagdon@gmail.com>
2014-12-15 22:31:04 -0800

    -daily code

--- diveintopython3/2.04.py
+++ diveintopython3/2.04.py
@@ -6,10 +6,23 @@ a_list[4]
 a_list[-1]
 a_list[-3]
 
-a_list
+# Slicing a list
 a_list[1:3]
 a_list[1:-1]
 a_list[0:3]
 a_list[:3]
 print (a_list[3:])
 print (a_list[:])
+a_list[1:3]
+a_list[1:-1]
+a_list[0:3]
+a_list[:3]
+a_list[3:]
+a_list[3]
+
+# adding items to a list
+a_list = a_list + [2.0, 3]
+a_list.append(True)
+a_list.extend(['four','Ω'])
+a_list.insert(0, 'Ω')
+a_list
