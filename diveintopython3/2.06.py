dd439e26029eabe285b250549868ad86ef11b8f8
karagdon <karagdon@gmail.com>
2014-12-23 23:43:19 -0800

    daily code

--- diveintopython3/2.06.py
+++ diveintopython3/2.06.py
@@ -13,3 +13,21 @@ a_list = ['a','b', 'mpilgrim', True, False, 42]
 a_set = set(a_list)
 a_set 	#{'a','b', 'mpilgrim', True, False, 42}
 a_list	#['a','b', 'mpilgrim', True, False, 42]
+
+
+k_list = ['python', 'chocolate', 'videogames']
+k_set = set(k_list)
+
+
+# an empty set
+a_set = set()
+a_set
+
+type(a_set) # <class 'set'>
+
+# modifying an empty set
+a_set.add(4) # adds any value of any type to the set
+a_set.update({2, 4, 6})
+a_set.discard(4)
+a_set.pop()
+a_set.clear
