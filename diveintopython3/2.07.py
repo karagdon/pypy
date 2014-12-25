1f796ee6b132bcdec5e01ce43eb34ba448628fac
karagdon <karagdon@gmail.com>
2014-12-25 14:45:24 -0800

    daily code

---
+++ diveintopython3/2.07.py
@@ -0,0 +1,11 @@
+# Dictionaries
+# # /Unordered set of key-value pairs, optimized for retrieving value when you know the key
+a_dict = {'server' : 'db.diveintopython3.org', 'database': 'mysql'}
+a_dict['server']
+a_dict['database']
+
+#modifying
+a_dict['database'] = 'blog'
+a_dict['user'] = 'k8'
+a_dict['user'] = 'day'
+a_dict['user'] = 'k8day'
