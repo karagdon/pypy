791f7a173a471fd43a8dec7d75c54cbca937cc96
karagdon <karagdon@gmail.com>
2014-12-26 19:30:13 -0800

    daily code

--- diveintopython3/2.07.py
+++ diveintopython3/2.07.py
@@ -9,3 +9,10 @@ a_dict['database'] = 'blog'
 a_dict['user'] = 'k8'
 a_dict['user'] = 'day'
 a_dict['user'] = 'k8day'
+
+#mixed value dictionaries
+
+SUFFIXES = {1000: ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+			1024: ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']}
+			
+len(SUFFIXES)
