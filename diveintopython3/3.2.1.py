9a3e962a165b00627c405544f6c981259bd9c506
karagdon <karagdon@gmail.com>
2014-12-30 23:56:46 -0800

    daily code

--- diveintopython3/3.2.1.py
+++ diveintopython3/3.2.1.py
@@ -7,3 +7,11 @@ print(os.getcwd())
 
 print(os.path.join('/var/host/media/removable/K8 PEARL/codex/pypy','humansize.py'))
 print(os.path.expanduser('~')) #function will expand a pathname that uses ~ to represent the current user’s home directory
+
+pathname = '/var/host/media/removable/K8 PEARL/codex/pypy/diveintopython3/humansize.py'
+os.path.split(pathname)
+(dirname, filename) = os.path.split(pathname)
+dirname
+filename
+(shortname, extension) = os.path.splitext(filename)
+shortname
