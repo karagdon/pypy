1adbedca366d72f674fff88047e6e4e8c78f55d5
karagdon <karagdon@gmail.com>
2015-01-07 23:13:07 -0800

    daily code

--- diveintopython3/3.03.py
+++ diveintopython3/3.03.py
@@ -11,3 +11,10 @@ glob.glob('*.xml')
 
 [(os.stat(f).st_size, os.path.realpath(f)) for f in glob.glob('*.xml')]
 #	This list comprehension finds all the .xml files in the current working directory, gets the size of each file (by calling the os.stat() function), and constructs a tuple of the file size and the absolute path of each file (by calling the os.path.realpath() function).
+
+# dictionary comprehensions
+metadata_dict = {f:os.stat(f) for f in glob.glob('*test*.py')}  ③
+ 
+# this is NOT dictionary comprehension
+metadata = [(f, os.stat(f)) for f in glob.glob('*test*.py')]
+metadata[0]
