db9352ab35384b97d18e480077379a7a0f7a39b2
karagdon <karagdon@gmail.com>
2015-01-14 23:25:33 -0800

    daily code

--- diveintopython3/00.py
+++ diveintopython3/00.py
@@ -1,1 +1,9 @@
-a_file = open('readmytext/test.txt', encoding='utf-8)'
+# take input (genre)
+# create a structure that stores a movie title with multiple genres
+# return titles with genre
+
+#use python to create an XML document that stores information
+
+import xml.etree.ElementTree as etree
+new_feed = etree.Element('{http://www.w3.org/2005/Atom}feed', attrib={'{http://www.w3.org/XML/1998/namespace}lang': 'en'})
+print(etree.tostring(new_feed))
