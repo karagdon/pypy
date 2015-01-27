dddb20ddb49707f60c2e87c83d90b3c2218e32c4
karagdon <karagdon@gmail.com>
2015-01-26 22:00:34 -0800

    daily code

--- watchMovie/addMovie.py
+++ watchMovie/addMovie.py
@@ -12,15 +12,9 @@ type(movies_dict.values())
 movies_dict[title] = genres
 
 for key, values in movies_dict.items():
-	print (key + "\t: " + str(values).strip('[\']'))
-#todo-- take off extra punctuation in lists
+	print (key + "\t: " + str(values).strip('[]'))
 	
-#print (', '.join(movies_dict.values()))
 
-
-#	print (k + "\t : " +  str(movies_dict[k]).strip('[]'))
-	
-"""
 # check if is in dict
 if title in movies_dict:
 	print ("Not added. That movie already exists, use -e to edit the movie")
@@ -31,4 +25,3 @@ else:
 	with open('movies.txt', mode='w', encoding='utf-8') as movieFile:
 		movieFile.write(str(movies_dict))
 		print ("add complete")
-"""
--- watchMovie/editMovie.py
+++ watchMovie/editMovie.py
@@ -1,2 +1,11 @@
-# remove the tuple
-# change the tuple
+# check if is in dict
+if title in movies_dict:
+	with open('movies.txt', mode='w', encoding='utf-8') as movieFile:
+		movieFile.write(str(movies_dict))
+		print ("add complete")
+
+
+#write to the file
+else:
+	print ("Not added. That doesnt exist in the database, use -a to add the movie with genres")
+	pass
