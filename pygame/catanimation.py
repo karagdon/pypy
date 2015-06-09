5a7964a5fc17fc79f1e756c30910aa46d871c4bb
karagdon <karagdon@gmail.com>
2015-06-08 23:02:04 -0700

    daily code

--- pygame/catanimation.py
+++ pygame/catanimation.py
@@ -6,11 +6,11 @@ pygame.init()
 FPS = 30
 fpsClock = pygame.time.Clock()
 
-DISPLAYSURF = pygame.display.dset_mode((400,300), 0, 32)
+DISPLAYSURF = pygame.display.set_mode((400,300), 0, 32)
 pygame.display.set_caption('Animation')
 
 WHITE = (255, 255, 255)
-catImg = pygame.image.load('cat.png')
+catImg = pygame.image.load('M3cXgre.jpg')
 catx = 10
 caty = 10
 direction = 'right'
